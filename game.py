027a44d9435dafc1f23bafd1ba35c2b6dacf4133
polnecha <polnecha2014@gmail.com>
2025-06-08 17:04:26 +0500

    Метод compare_cards. Сравнивает две карты с правилом, что 0 бьёт 9, возвращает победителя (1 или 2).

--- game.py
+++ game.py
@@ -3,3 +3,15 @@ class CardGame:
         self.player1 = list(player1_cards)
         self.player2 = list(player2_cards)
         self.max_rounds = 10 ** 6
+
+    def compare_cards(self, c1, c2):
+        """Сравнивает две карты с учетом правила '0 бьет 9'."""
+        if c1 == 0 and c2 == 9:
+            return 1
+        elif c1 == 9 and c2 == 0:
+            return 2
+        elif c1 > c2:
+            return 1
+        else:
+            return 2
+
